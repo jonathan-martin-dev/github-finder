940e0b327fbbbd05fcb1439254ce09ddd2f2f520
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-05-04 17:16:35 -0700

    test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,8 @@ const About = () => (
     <>
         <h1 className="text-6xl mb-4">Github Finder</h1>
         <p className="mb-4 text-2xl font-light">
-            A React app to search GitHub profiles and see profile details. This
-            project is part of the
+            A React test app to search GitHub profiles and see profile details.
+            This project is part of the
             <a href="https://www.udemy.com/course/modern-react-front-to-back/">
                 {' '}
                 React Front To Back
