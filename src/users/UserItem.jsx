282a4433bb7e0ed7cb384cbc92445c9f86cc3e4c
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-05-07 14:24:19 -0700

    context and reducers for users and losing added

--- src/users/UserItem.jsx
+++ src/users/UserItem.jsx
@@ -16,7 +16,10 @@ const UserItem = ({ user: { login, avatar_url } }) => (
             </div>
             <div>
                 <h2 className="card-title">{login}</h2>
-                <Link className="text-base-content text-opacity-40" to="/">
+                <Link
+                    className="text-base-content text-opacity-40"
+                    to={`/users/${login}`}
+                >
                     Visit Profile
                 </Link>
             </div>
